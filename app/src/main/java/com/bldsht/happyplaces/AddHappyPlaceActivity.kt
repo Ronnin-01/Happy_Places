2eeaf64a7dcff20ea35f48ec9d6790ed77623956
Ronnin-01 <omarmd425@gmail.com>
2024-06-25 20:27:35 +0530

    Added Toolbar

--- app/src/main/java/com/bldsht/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/bldsht/happyplaces/AddHappyPlaceActivity.kt
@@ -5,11 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bldsht.happyplaces.databinding.ActivityAddHappyPlaceBinding
 
 class AddHappyPlaceActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddHappyPlaceBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_happy_place)
-
+        //setContentView(R.layout.activity_add_happy_place)
+        binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        //setSupportActionBar(toolbar_add_place)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.toolbarAddPlace.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
     }
 }
