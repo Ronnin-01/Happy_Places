f8807ff40750412be0b7847a95ee52a5c8bb79e0
Ronnin-01 <omarmd425@gmail.com>
2024-06-25 14:39:41 +0530

    Started working in it 2

--- app/src/main/java/com/bldsht/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/bldsht/happyplaces/AddHappyPlaceActivity.kt
@@ -10,5 +10,6 @@ class AddHappyPlaceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
+
     }
 }
