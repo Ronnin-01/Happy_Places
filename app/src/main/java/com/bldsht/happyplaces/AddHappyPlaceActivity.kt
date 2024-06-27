252c2ffd6f22fcd7037f2d535762d22d0d31e09a
Ronnin-01 <omarmd425@gmail.com>
2024-06-27 18:01:22 +0530

    calender

--- app/src/main/java/com/bldsht/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/bldsht/happyplaces/AddHappyPlaceActivity.kt
@@ -1,5 +1,7 @@
 package com.bldsht.happyplaces
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,7 +10,11 @@ import androidx.core.view.WindowInsetsCompat
 import com.bldsht.happyplaces.databinding.ActivityAddHappyPlaceBinding
 
 class AddHappyPlaceActivity : AppCompatActivity() {
+
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
     private lateinit var binding: ActivityAddHappyPlaceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_add_happy_place)
@@ -19,6 +25,14 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         binding.toolbarAddPlace.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
+        }
+
+        dateSetListener = DatePickerDialog.OnDateSetListener{
+            view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
         }
     }
 }
