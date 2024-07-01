7277a78c136f1aa531e82c357aec6eea51d21a65
Ronnin-01 <omarmd425@gmail.com>
2024-07-01 17:35:04 +0530

    successfully getting the db data

--- app/src/main/java/com/bldsht/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/bldsht/happyplaces/activities/MainActivity.kt
@@ -2,8 +2,11 @@ package com.bldsht.happyplaces.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.bldsht.happyplaces.database.DataBaseHandler
 import com.bldsht.happyplaces.databinding.ActivityMainBinding
+import com.bldsht.happyplaces.models.HappyPlaceModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,6 +19,19 @@ class MainActivity : AppCompatActivity() {
         binding.fabAddHappyPlace.setOnClickListener {
             val intent = Intent(this@MainActivity, AddHappyPlaceActivity::class.java)
             startActivity(intent)
+        }
+        getHappyPlacesListFromLocalDB()
+    }
+
+    private fun getHappyPlacesListFromLocalDB(){
+        val dbHandler = DataBaseHandler(this)
+        val getHappyPlaceList : ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
+
+        if (getHappyPlaceList.size > 0 ){
+            for(i in getHappyPlaceList){
+                Log.e("Title",i.title)
+                Log.e("desc",i.description)
+            }
         }
     }
 }
--- app/src/main/java/com/bldsht/happyplaces/database/DataBaseHandler.kt
+++ app/src/main/java/com/bldsht/happyplaces/database/DataBaseHandler.kt
@@ -85,8 +85,10 @@ class DataBaseHandler (context: Context): SQLiteOpenHelper(context, DATABASE_NAM
                         cursor.getDouble(cursor.getColumnIndex(KEY_LATITUDE)),
                         cursor.getDouble(cursor.getColumnIndex(KEY_LONGITUDE))
                         )
+                    happyPlaceList.add(place)
                 }while (cursor.moveToNext())
             }
+            cursor.close()
         }catch (e: SQLiteException){
             db.execSQL(selectQuery)
             return ArrayList()
